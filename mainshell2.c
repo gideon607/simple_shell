eadcf81942e957f3e6a7670918541381cb412e7d
Michael Alem <64676354+X-C0DER@users.noreply.github.com>
2023-07-26 18:11:02 +0300

    Update mainshell2.c

--- mainshell2.c
+++ mainshell2.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
 * replace_variables - replaces variables in a line of text
 * @line: line of text
@@ -9,26 +7,26 @@
 * Return: void
 */
 void replace_variables(char *line) {
-    char *var;
-    char buffer[MAXLINE];
-    pid_t pid;
+        char *var;
+        char buffer[MAXLINE];
+        pid_t pid;
 
-    var = strstr(line, "$?");
-    if (var != NULL) {
-        sprintf(buffer, "%d", last_exit_status);
-        strncpy(var, buffer, strlen(buffer));
-        var += strlen(buffer);
-        *var = ' ';
-    }
-
-    var = strstr(line, "$$");
-    if (var != NULL) {
-        pid = getpid();
-        sprintf(buffer, "%d", pid);
-        strncpy(var, buffer, strlen(buffer));
-        var += strlen(buffer);
-        *var = ' ';
-    }
+        var = strstr(line, "$?");
+        if (var != NULL)
+        {
+                sprintf(buffer, "%d", last_exit_status);
+                strncpy(var, buffer, strlen(buffer));
+                var += strlen(buffer);
+                *var = ' ';
+        }
+        var = strstr(line, "$$");
+        if (var != NULL) {
+                pid = getpid();
+                sprintf(buffer, "%d", pid);
+                strncpy(var, buffer, strlen(buffer));
+                var += strlen(buffer);
+                *var = ' ';
+        }
 }
 
 /**
@@ -39,7 +37,7 @@ void replace_variables(char *line) {
 */
 void execute_exit(char **args)
 {
-    exit(args[1] != NULL ? _atoi(args[1]) : EXIT_SUCCESS);
+        exit(args[1] != NULL ? _atoi(args[1]) : EXIT_SUCCESS);
 }
 
 /**
@@ -50,27 +48,27 @@ void execute_exit(char **args)
 */
 void execute_cd(char **args)
 {
-    char *dir;
-    char cwd[MAXLINE];
+        char *dir;
+        char cwd[MAXLINE];
 
-    if (args[1] == NULL)
-        dir = getenv("HOME");
-    else if (strcmp(args[1], "-") == 0)
-    {
-        dir = getenv("OLDPWD");
-        printf("%s\n", dir);
-    }
-    else
-        dir = args[1];
+        if (args[1] == NULL)
+                dir = getenv("HOME");
+        else if(strcmp(args[1], "-") == 0)
+        {
+                dir = getenv("OLDPWD");
+                printf("%s\n", dir);
+        }
+        else
+                dir = args[1];
 
-    if (chdir(dir) != 0)
-        perror("cd");
+        if (chdir(dir) != 0)
+                perror("cd");
 
-    if (getcwd(cwd, sizeof(cwd)) != NULL)
-    {
-        setenv("OLDPWD", getenv("PWD"), 1);
-        setenv("PWD", cwd, 1);
-    }
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+        {
+                setenv("OLDPWD", getenv("PWD"), 1);
+                setenv("PWD", cwd, 1);
+        }
 }
 
 /**
@@ -81,9 +79,9 @@ void execute_cd(char **args)
 */
 void execute_env(void)
 {
-    int i;
-    for (i = 0; environ[i] != NULL; i++)
-        printf("%s\n", environ[i]);
+        int i;
+        for (i = 0; environ[i] != NULL; i++)
+                printf("%s\n", environ[i]);
 }
 
 /**
@@ -94,11 +92,8 @@ void execute_env(void)
 */
 void execute_setenv(char **args)
 {
-    if (args[1] == NULL || args[2] == NULL)
-        fprintf(stderr, "setenv: missing arguments\n");
-    else if (setenv(args[1], args[2], 1) != 0)
-        perror("setenv");
+        if (args[1] == NULL || args[2] == NULL)
+                fprintf(stderr, "setenv: missing arguments\n");
+        else if (setenv(args[1], args[2], 1) != 0)
+                perror("setenv");
 }
-
-
-
