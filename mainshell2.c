a532feea53616fcf1dfd9a19707af6f294cd9a68
Michael Alem <64676354+X-C0DER@users.noreply.github.com>
2023-07-26 13:57:10 +0300

    Update mainshell2.c

--- mainshell2.c
+++ mainshell2.c
@@ -1,85 +1,72 @@
 #include "main.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <dirent.h>
 
-#define MAX_COMMAND_PER_LINE 50
-/**
- * exe_commands - main exection command
- * @commands_line: command to be done
- * Return: is 0 (for success)
- */
-void exe_commands(char *commands_line)
-{
-	pid_t pid = fork(); /* for the new process id */
+void replace_variables(char *line) {
+    char *var;
+    char buffer[MAXLINE];
+    pid_t pid;
 
-	if (pid == 0)
-	{
-		/* for within process */
-		char *token;
-		char *args[MAX_COMMAND_PER_LINE];
-		int arg_count = 0;
+    var = strstr(line, "$?");
+    if (var != NULL) {
+        sprintf(buffer, "%d", last_exit_status);
+        strncpy(var, buffer, strlen(buffer));
+        var += strlen(buffer);
+        *var = ' ';
+    }
 
-		token = strtok(commands_line, " \t\n"); /* into token */
-		while (token != NULL && arg_count < MAX_COMMAND_PER_LINE - 1)
-		{
-			args[arg_count] = token;
-			arg_count++;
-			token = strtok(NULL, "\t\n");
-		}
-		args[arg_count] = NULL;
-		execve(args[0], args, NULL); /* execution on the command */
-		perror("404ERROR"); /* prints the errormsg */
-		_exit(EXIT_FAILURE);
-	} else if (pid > 0)
-	{ /* main process */
-		int main_status;
-
-		waitpid(pid, &main_status, 0); /* wait command for child */
-	} else
-	{
-		perror("404ERROR"); /* pick fork process failed */
-		exit(EXIT_FAILURE);
-	}
+    var = strstr(line, "$$");
+    if (var != NULL) {
+        pid = getpid();
+        sprintf(buffer, "%d", pid);
+        strncpy(var, buffer, strlen(buffer));
+        var += strlen(buffer);
+        *var = ' ';
+    }
 }
 
-/**
- * main - entry on the program
- *
- * Return: is always 0.
- */
-int main(void)
+void execute_exit(char **args)
 {
-	char commands[MAX_COMMAND_PER_LINE];
-	size_t commands_per_line;
-
-	while (1)
-	{
-		printf("## "); /* Start of commandline */
-		if (fgets(commands, sizeof(commands), stdin) == NULL)
-		{
-			printf("\nOuttahere.....\n");
-			break;
-		}
-		commands_per_line = strlen(commands);
-
-		if (commands[commands_per_line - 1] == '\n')
-		{
-			commands[commands_per_line - 1] = '\0'; /* newline erase */
-		}
-
-		if (strlen(commands) == 0)
-		{
-			continue;
-		}
-		exe_commands(commands);
-	}
-	return (0);
+    exit(args[1] != NULL ? _atoi(args[1]) : EXIT_SUCCESS);
 }
+
+void execute_cd(char **args)
+{
+    char *dir;
+    char cwd[MAXLINE];
+
+    if (args[1] == NULL)
+        dir = getenv("HOME");
+    else if (strcmp(args[1], "-") == 0)
+    {
+        dir = getenv("OLDPWD");
+        printf("%s\n", dir);
+    }
+    else
+        dir = args[1];
+
+    if (chdir(dir) != 0)
+        perror("cd");
+
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        setenv("OLDPWD", getenv("PWD"), 1);
+        setenv("PWD", cwd, 1);
+    }
+}
+
+void execute_env(char **args)
+{
+    int i;
+    for (i = 0; environ[i] != NULL; i++)
+        printf("%s\n", environ[i]);
+}
+
+void execute_setenv(char **args)
+{
+    if (args[1] == NULL || args[2] == NULL)
+        fprintf(stderr, "setenv: missing arguments\n");
+    else if (setenv(args[1], args[2], 1) != 0)
+        perror("setenv");
+}
+
+
 
