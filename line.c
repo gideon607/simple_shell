2696185234b1a5800935f4c023c6b450b1b9e89c
Michael Alem <64676354+X-C0DER@users.noreply.github.com>
2023-07-26 20:46:24 +0300

    Update line.c

--- line.c
+++ line.c
@@ -72,39 +72,3 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 	return (total_len > 0 ? (ssize_t)total_len : bytes_read);
 
 }
-
-/**
-* _atoi - converts a string to an integer
-* @s: input string
-*
-* Return: integer value represented by the string
-*/
-int _atoi(char *s)
-{
-	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
-
-	while (*(s + count) != '\0')
-	{
-		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-			break;
-		if (*(s + count) == '-')
-			pn *= -1;
-
-		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
-		{
-			if (size > 0)
-			{
-				m *= 10;
-				size++;
-			}
-		}
-		count++;
-	}
-	for (i = count - size; i < count; i++)
-	{
-		oi = oi + ((*(s + i) - 48) * m);
-		m /= 10;
-	}
-	return (oi * pn);
-}
-
