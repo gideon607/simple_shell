a522ce414e6dbe267f3dff30e4fe1b3193511446
Michael Alem <64676354+X-C0DER@users.noreply.github.com>
2023-07-27 13:44:38 +0300

    Update mainshell2.c

--- mainshell2.c
+++ mainshell2.c
@@ -1,101 +1,42 @@
 #include "main.h"
 
 /**
-* replace_variables - replaces variables in a line of text
-* @line: line of text
-*
-* Return: void
-*/
-void replace_variables(char *line)
+  * *search_path - makes a complete path from path.
+  * @cmd: command to make complete path from.
+  * Return: complete path.
+  */
+char *search_path(char *cmd)
 {
-	char *var;
-	char buffer[MAXLINE];
-	pid_t pid;
-	var = strstr(line, "$?");
-
-	if (var != NULL)
-	{
-		sprintf(buffer, "%d", last_exit_status);
-		strncpy(var, buffer, strlen(buffer));
-		var += strlen(buffer);
-		*var = ' ';
-	}
-	var = strstr(line, "$$");
-	if (var != NULL)
-	{
-	pid = getpid();
-	sprintf(buffer, "%d", pid);
-	strncpy(var, buffer, strlen(buffer));
-	var += strlen(buffer);
-	*var = ' ';
-	}
-}
-
-/**
-* execute_exit - executes the exit command
-* @args: array of arguments
-*
-* Return: void
-*/
-void execute_exit(char **args)
-{
-	exit(args[1] != NULL ? _atoi(args[1]) : EXIT_SUCCESS);
-}
-
-/**
-* execute_cd - executes the cd command
-* @args: array of arguments
-*
-* Return: void
-*/
-void execute_cd(char **args)
-{
-	char *dir;
-	char cwd[MAXLINE];
-
-	if (args[1] == NULL)
-		dir = getenv("HOME");
-	else if (strcmp(args[1], "-") == 0)
-	{
-		dir = getenv("OLDPWD");
-		printf("%s\n", dir);
-	}
-	else
-		dir = args[1];
-
-	if (chdir(dir) != 0)
-	perror("cd");
-
-	if (getcwd(cwd, sizeof(cwd)) != NULL)
-	{
-		setenv("OLDPWD", getenv("PWD"), 1);
-		setenv("PWD", cwd, 1);
-	}
-}
-
-/**
-* execute_env - executes the env command
-*
-*
-* Return: void
-*/
-void execute_env(void)
-{
+	char *path = NULL, *token = NULL, ch = '/';
+	char *dirs[1024];
 	int i;
-	for (i = 0; environ[i] != NULL; i++)
-		printf("%s\n", environ[i]);
-}
+	char *cwd = getcwd(NULL, 0);
+	struct stat sb;
 
-/**
-* execute_setenv - executes the setenv command
-* @args: array of arguments
-*
-* Return: void
-*/
-void execute_setenv(char **args)
-{
-	if (args[1] == NULL || args[2] == NULL)
-		fprintf(stderr, "setenv: missing arguments\n");
-	else if (setenv(args[1], args[2], 1) != 0)
-		perror("setenv");
+	path = getenv("PATH");
+	token = strtok(path, ":");
+	if (dirs == NULL)
+	i = 0;
+	while (token != NULL)
+	{
+		dirs[i] = token;
+		token = strtok(NULL, ":");
+		i++;
+	}
+	dirs[i] = NULL;
+
+	i = 0;
+	while (dirs[i] != NULL)
+	{
+		chdir(dirs[i]);
+		if (stat(cmd, &sb) == 0)
+		{
+			dirs[i] = concat_str(dirs[i], &ch, 1);
+			cmd = str_concat(dirs[i], cmd);
+			break;
+		}
+		i++;
+	}
+	chdir(cwd);
+	return (cmd);
 }
