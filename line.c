ed10b58be7c471c20b8a7c84bd1e5200d20fb2a7
Michael Alem <64676354+X-C0DER@users.noreply.github.com>
2023-07-27 13:39:53 +0300

    Update line.c

--- line.c
+++ line.c
@@ -1,100 +1,108 @@
 #include "main.h"
 
-#define BUFFER_SIZE 1024
-
 /**
-* parse_line - splits a line into tokens separated by spaces
-* @line: line to split
-* @args: array of pointers to strings to store tokens
-*
-* Return: void
-*/
-void parse_line(char *line, char **args)
+  * **parse_strings - splits the string entered by the user.
+  * @buffer: string to be split.
+  * Return: splitted string.
+  */
+char **parse_strings(char *buffer)
 {
-	int i = 0;
-	args[0] = _strtok(line, " ");
+	char **cmd = NULL;
+	char *token, *delimeter = " \n";
+	int i;
 
-	while (args[i] != NULL)
-		args[++i] = _strtok(NULL, " ");
+	cmd = malloc(sizeof(char *) * 1024);
+	if (cmd == NULL)
+	{
+		perror("memory could not be allocated");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(buffer, delimeter);
+	i = 0;
+	while (token != NULL)
+	{
+		cmd[i] = token;
+		token = strtok(NULL, delimeter);
+		i++;
+	}
+	cmd[i] = NULL;
+	return (cmd);
 }
 
 /**
-* _getline - reads an entire line from a file stream
-* @lineptr: pointer to the buffer that stores the line
-* @n: size of the buffer
-* @stream: file stream to read from
-*
-* Return: number of characters read, including newline character, but not
-* including terminating null byte. -1 on error or end-of-file.
-*/
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
-{
-	static char buffer[BUFFER_SIZE];
-	static size_t start=0;
-	static size_t end=0;
-	size_t total_len=0;
-	ssize_t bytes_read;
-
-	if (lineptr == NULL || n == NULL || stream == NULL)
-		return (-1);
-	if (*lineptr == NULL)
-	{
-		*lineptr = malloc(BUFFER_SIZE);
-		if (*lineptr == NULL)
-			return (-1);
-
-		*n = BUFFER_SIZE;
-	}
-
-	while (1)
-	{
-	if (start >= end)
-	{
-		bytes_read = read(fileno(stream), buffer, BUFFER_SIZE);
-		if (bytes_read <= 0)
-			break;
-		start = 0;
-		end = bytes_read;
-	}
-	if (total_len >= *n - 1)
-	{
-		*n *= 2;
-		*lineptr = realloc(*lineptr, *n);
-		if (*lineptr == NULL)
-			return (-1);
-	}
-
-	(*lineptr)[total_len++] = buffer[start++];
-	if ((*lineptr)[total_len - 1] == '\n')
-		break;
-	}
-	(*lineptr)[total_len] = '\0';
-	return (total_len > 0 ? (ssize_t)total_len : bytes_read);
-	
-}
-
-/**
- * _strtok - splits a string into tokens based on a delimiter
- * @str: the string to split
- * @delim: the delimiter to split the string on
+ * *str_concat - concatenates two strings.
  *
- * Return: pointer to the first token found in the string.
- * NULL is returned when there are no more tokens.
+ * @dest: resulting string.
+ * @src: source string.
+ * Return: pointer to dest
  */
-char *_strtok(char *str, const char *delim)
+char *str_concat(char *dest, char *src)
 {
-	static char *lasts;
-	int ch;
+	int i, n;
 
-	if (str == 0)
-		str = lasts;
-	do {
-		if ((ch = *str++) == '\0')
-			return (0);
-	} while (strchr(delim, ch));
-	--str;
-	lasts = str + strcspn(str, delim);
-	if (*lasts != 0)
-		*lasts++ = 0;
-	return (str);
+	n = find_len(dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[n + i] = src[i];
+	}
+	dest[n + i] = '\0';
+	return (dest);
+}
+
+/**
+ * *locate_char - locates a character in a string.
+ *
+ * @s: string.
+ * @c: character to be located.
+ * Return: pointer to first occurrence of NULL if no character found.
+ */
+char *locate_char(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
+}
+
+/**
+  * find_len - finds the length of a string.
+  * Return: length of c.
+  * @pointer: pointer.
+  */
+int find_len(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
+
+/**
+ * concat_str - concatenates two strings.
+ *
+ * @dest: resulting string.
+ * @src: source string.
+ * @n: string length.
+ * Return: pointer to dest
+ */
+
+char *concat_str(char *dest, char *src, int n)
+{
+	int i, m;
+
+	m = find_len(dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[m + i] = src[i];
+	}
+	dest[m + i] = '\0';
+	return (dest);
 }
